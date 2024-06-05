18630db1bc59135be3255b17a03f82a7c93869e7
chase <chase_si@163.com>
2024-06-05 14:40:50 +0800

    [feat]add pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'nextjs-app',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start -p 3013',
+      instances: 1, // 使用多核模式，可以设置为1以使用单一实例
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }
+  ]
+};
